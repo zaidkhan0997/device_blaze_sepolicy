301898ac87b4a610244e81f80da686b3c2d2c3ad
Pratyaksh Bharadwaj <pratyakshom.05@gmail.com>
2022-03-10 07:55:32 -0600

    SEPolicy: Adapt for Blaze
    Signed-off-by: Pratyaksh Bharadwaj <pratyakshom.05@gmail.com>

--- legacy-common/sepolicy.mk
+++ legacy-common/sepolicy.mk
@@ -1,9 +1,9 @@
 #
 # This policy configuration will be used by select legacy products that
-# inherit from Lineage
+# inherit from Blaze
 #
 
 ifeq ($(TARGET_HAS_LEGACY_CAMERA_HAL1), true)
 SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += \
-    device/octavi/sepolicy/legacy-common/public/legacy-camera-hal1
+    device/blaze/sepolicy/legacy-common/public/legacy-camera-hal1
 endif
