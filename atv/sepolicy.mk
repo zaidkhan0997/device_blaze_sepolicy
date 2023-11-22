f551abc37b4927d8d2aaeb0e7b8c2588666028b7
afterallafk <shivamatiet2001@gmail.com>
2023-11-22 18:01:31 +0545

    sepolicy: Adapt for ProjectBlaze
    Change-Id: I734f370d1a0db02b0e674de9efb6e5ae443fb530
    Signed-off-by: saikiran2001 <bjsaikiran@gmail.com>

--- atv/sepolicy.mk
+++ atv/sepolicy.mk
@@ -5,8 +5,8 @@
 
 ifneq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
 BOARD_SEPOLICY_DIRS += \
-    device/lineage/sepolicy/atv/vendor
+    device/blaze/sepolicy/atv/vendor
 endif
 
 PRODUCT_PRIVATE_SEPOLICY_DIRS += \
-   device/lineage/sepolicy/atv/private
+   device/blaze/sepolicy/atv/private
--- qcom/sepolicy.mk
+++ qcom/sepolicy.mk
@@ -10,16 +10,16 @@ endif
 endif
 
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
-    device/lineage/sepolicy/qcom/private
+    device/blaze/sepolicy/qcom/private
 
 ifeq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
-    device/lineage/sepolicy/qcom/dynamic \
-    device/lineage/sepolicy/qcom/system
+    device/blaze/sepolicy/qcom/dynamic \
+    device/blaze/sepolicy/qcom/system
 else
 BOARD_VENDOR_SEPOLICY_DIRS += \
-    device/lineage/sepolicy/qcom/dynamic \
-    device/lineage/sepolicy/qcom/vendor
+    device/blaze/sepolicy/qcom/dynamic \
+    device/blaze/sepolicy/qcom/vendor
 endif
 
 ifeq (,$(filter msm8937 msm8953 msm8996 msm8998 sdm660 sdm710 sdm845, $(TARGET_BOARD_PLATFORM)))
